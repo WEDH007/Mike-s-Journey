c87eaf07952cdbbc1c32853b253c9e79675e9d68
MikePena <MikePena@NAV-MPENA-LP>
2024-11-22 13:59:08 -0600

    First commit.

---
+++ Loops/WhileLoops/Program.cs
@@ -0,0 +1,12 @@
+﻿/*2. Number Guessing Game (While Loop)
+Description: Build a game where the computer generates a random number between 1 and 100, and the player has to guess it. Each time the player guesses incorrectly, the program gives a hint whether the guess is too high or too low.
+
+Requirements:
+
+Use a while loop that runs until the player guesses the correct number.
+After each guess, the program should inform the player if the guess is too high or too low.
+Keep track of the number of attempts and display it when the player guesses correctly.
+Allow the player to quit the game early (ask if they want to keep playing after each guess).
+*/
+
+
