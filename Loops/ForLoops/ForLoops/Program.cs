7ffcb079e7a987ca09a9e9bc40acbcf97cba319c
MikePena <MikePena@NAV-MPENA-LP>
2024-11-25 10:13:02 -0600

    Updated.

---
+++ Loops/ForLoops/ForLoops/Program.cs
@@ -0,0 +1,49 @@
+﻿
+int factors = 0;
+
+Console.Write("Write a limit\n");
+
+int limit = int.Parse(Console.ReadLine());
+
+for (int i = 2; i < limit; i++)
+{
+    factors = 0;
+    for (int n = 1; n < limit; n++)
+    {
+       
+        if (0 == i % n)
+            
+        {
+            factors = factors + 1;
+        }
+        
+    }
+    if (factors == 2)
+    {
+        Console.WriteLine(i);
+    }
+};
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
