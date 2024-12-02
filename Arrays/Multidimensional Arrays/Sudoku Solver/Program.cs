4ac6d7ba271cdce6979c134ded9ac06d5b517abe
MikePena <MikePena@NAV-MPENA-LP>
2024-12-02 15:57:22 -0600

    Intro

--- Arrays/Multidimensional Arrays/Sudoku Solver/Program.cs
+++ Arrays/Multidimensional Arrays/Sudoku Solver/Program.cs
@@ -1,12 +1,19 @@
 ﻿
 //7. 2D Array Search
-//Description: Implement a program that will let you reserve a plane seat. 
+//Description: Implement a program that will let you reserve a specific plane seat. 
+//Program asks to assign seats on a small airplane that has 7 rows and 4 columns. after a seat has been selected by user input, an X is to appear in that spot and the new display is to be displayed
+//user input letter A,B,C,D and a number.
 
+Console.WriteLine("Welcome To UnitedAirlines");
+char[,] seats = {
+{ 'A', 'B', 'C', 'D' },//0
+{ 'A', 'B', 'C', 'D' },//1
+{ 'A', 'B', 'C', 'D' },//2
+{ 'A', 'X', 'C', 'D' },//3
+{ 'A', 'B', 'C', 'D' },//4
+{ 'A', 'B', 'C', 'D' },//5
+{ 'A', 'B', 'C', 'D' },//6
+};
 
-
-
-
-int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
-Console.WriteLine("Insert the value you want to find");
-while(int.TryParse())
-
+Console.WriteLine(seats[3, 1]);
+Console.ReadLine();
