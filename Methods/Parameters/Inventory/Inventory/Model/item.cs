c3bb5a1a3db47c0901d0ff89203ff0a91e9a7d9c
MikePena <MikePena@NAV-MPENA-LP>
2024-12-06 14:37:29 -0600

    Code Clean up, plus new methods.

--- Methods/Parameters/Inventory/Inventory/Model/item.cs
+++ Methods/Parameters/Inventory/Inventory/Model/item.cs
@@ -1,21 +1,19 @@
-﻿
-
-namespace Inventory
+﻿namespace Inventory.Model
 {
     class Item
     {
-        public int Id { get;}
-        public string Name { get; set;}
+        public int Id { get; }
+        public string Name { get; set; }
         public int Quantity { get; set; }
         public int Price { get; set; }
-        
+
         public Item(string name, int quantity, int price)
         {
             Name = name;
-            Quantity = quantity; 
+            Quantity = quantity;
             Price = price;
         }
 
-       
+
     }
 }
